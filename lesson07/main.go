851afeaf36650204a823052dd778642b6a3c3370
suming <suming22x@163.com>
2025-07-14 23:17:49 +0800

    note: learn go-web

--- lesson07/main.go
+++ lesson07/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request){
+func index(w http.ResponseWriter, r *http.Request) {
 	// 定义模板
 	// 解析模板
 	t, err := template.ParseFiles("./index.tmpl")
@@ -19,7 +19,7 @@ func index(w http.ResponseWriter, r *http.Request){
 	t.Execute(w, msg)
 }
 
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
 	// 定义模板
 	// 解析模板
 	t, err := template.ParseFiles("./home.tmpl")
@@ -32,7 +32,7 @@ func home(w http.ResponseWriter, r *http.Request){
 	t.Execute(w, msg)
 }
 
-func index2(w http.ResponseWriter, r *http.Request){
+func index2(w http.ResponseWriter, r *http.Request) {
 	// 定义模板（模板继承的方式）
 	// 解析模板
 	t, err := template.ParseFiles("./templates/base.tmpl", "./templates/index2.tmpl")
@@ -44,7 +44,7 @@ func index2(w http.ResponseWriter, r *http.Request){
 	name := "小王子"
 	t.ExecuteTemplate(w, "index2.tmpl", name)
 }
-func home2(w http.ResponseWriter, r *http.Request){
+func home2(w http.ResponseWriter, r *http.Request) {
 	// 定义模板（模板继承的方式）
 	// 解析模板
 	t, err := template.ParseFiles("./templates/base.tmpl", "./templates/home2.tmpl")
@@ -56,7 +56,6 @@ func home2(w http.ResponseWriter, r *http.Request){
 	name := "七米"
 	t.ExecuteTemplate(w, "home2.tmpl", name)
 }
-
 
 func main() {
 	http.HandleFunc("/index", index)

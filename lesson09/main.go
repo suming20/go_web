de0faaf93d149db9eb3e53327ec95ed1b91c89f1
suming <suming22x@163.com>
2025-07-16 23:17:52 +0800

    note: learn go-web

--- lesson09/main.go
+++ lesson09/main.go
@@ -15,23 +15,23 @@ func main() {
 
 	// gin框架中给模板添加自定义函数
 	r.SetFuncMap(template.FuncMap{
-		"safe": func(str string) template.HTML{
+		"safe": func(str string) template.HTML {
 			return template.HTML(str)
 		},
 	})
-	//r.LoadHTMLFiles("templates/index.tmpl", "templates/users/index.tmpl")  // 模板解析
+	// r.LoadHTMLFiles("templates/index.tmpl", "templates/users/index.tmpl")  // 模板解析
 	r.LoadHTMLGlob("templates/**/*")
 
 	r.GET("/posts/index", func(c *gin.Context) {
 		// HTTP请求
-		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{  // 模板渲染
+		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{ // 模板渲染
 			"title": "liwenzhou.com",
 		})
 	})
 
 	r.GET("/users/index", func(c *gin.Context) {
 		// HTTP请求
-		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{  // 模板渲染
+		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{ // 模板渲染
 			"title": "<a href='https://liwenzhou.com'>李文周的博客</a>",
 		})
 	})

a60b0d6559d1226e3693244732ea5a10c0038ea4
suming <suming22x@163.com>
2025-07-25 23:33:52 +0800

    note: learn go-web

--- lesson22/main.go
+++ lesson22/main.go
@@ -5,13 +5,14 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
 // gorm demo04
 
 // 1. 定义模型
 type User struct {
-	gorm.Model  // ID CreateAt UpdateAt DeleteAt
-	Name string
-	Age int64
+	gorm.Model // ID CreateAt UpdateAt DeleteAt
+	Name       string
+	Age        int64
 }
 
 func main() {
@@ -25,24 +26,24 @@ func main() {
 	db.AutoMigrate(&User{})
 
 	// 3. 创建
-	//u1 := User{Name: "q1mi", Age:18}
-	//db.Create(&u1)
-	//u2 := User{Name:"jinzhu", Age:20}
-	//db.Create(&u2)
+	// u1 := User{Name: "q1mi", Age:18}
+	// db.Create(&u1)
+	// u2 := User{Name:"jinzhu", Age:20}
+	// db.Create(&u2)
 
 	// 4. 查询
-	//var user User  // 声明模型结构体类型变量user
-	//user := new(User)  // new和make的区别
-	//db.First(user)
-	//fmt.Printf("user:%#v\n", user)
+	// var user User  // 声明模型结构体类型变量user
+	// user := new(User)  // new和make的区别
+	// db.First(user)
+	// fmt.Printf("user:%#v\n", user)
 	//
-	//var users []User
-	//db.Debug().Find(&users)
-	//fmt.Printf("users:%#v\n", users)
+	// var users []User
+	// db.Debug().Find(&users)
+	// fmt.Printf("users:%#v\n", users)
 
 	// FirstOrInit
 	var user User
-	//db.Attrs(User{Age:99}).FirstOrInit(&user, User{Name: "jinzhu"})
-	db.Assign(User{Age:99}).FirstOrInit(&user, User{Name: "jinzhu"})
+	// db.Attrs(User{Age:99}).FirstOrInit(&user, User{Name: "jinzhu"})
+	db.Assign(User{Age: 99}).FirstOrInit(&user, User{Name: "jinzhu"})
 	fmt.Printf("user:%#v\n", user)
 }
